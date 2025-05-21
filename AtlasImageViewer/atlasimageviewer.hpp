54bfde5834caf00ddae46beebd1c971d4c510003
Sailanarmo <rlramirez12@gmail.com>
2025-05-21 14:44:45 -0600

    Fixed broken code.

--- AtlasImageViewer/atlasimageviewer.hpp
+++ AtlasImageViewer/atlasimageviewer.hpp
@@ -60,6 +60,7 @@ namespace AtlasImageViewer
       auto CreateNewContext() -> std::unique_ptr<QOpenGLContext>;
       auto CreateTexture(const QImage& image, QOpenGLFunctions* gl_funcs) -> GLuint;
       auto CreateFrameBuffer(const QSize size) -> std::unique_ptr<QOpenGLFramebufferObject>;
+      auto DrawToFBO(QOpenGLFramebufferObject* fbo, QOpenGLFunctions* gl_funcs, const GLuint textureId) -> void;
       auto AddFBOToArray(std::unique_ptr<QOpenGLFramebufferObject>&& fbo, const double weight) -> void;
       
   };
