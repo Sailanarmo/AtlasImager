df1933a93c7d71de1487b5469d583c1caadb7081
Jared <jared.g.gibson32@gmail.com>
2025-05-22 14:26:35 -0600

    Remove thirdparty/opencv from git

--- cmake-build-debug/CMakeFiles/d/02856f91a464f92a011df5dbc3402ee667f0089e082f47b58ccae1d662089328.d
+++
@@ -1,5 +0,0 @@
-AtlasImage/Image_autogen/timestamp: \
-  /Applications/CLion.app/Contents/bin/cmake/mac/aarch64/bin/cmake \
-  /Users/jgibb/Documents/Spring_2025/Research/AtlasImager/AtlasImage/CMakeLists.txt \
-  /Users/jgibb/Documents/Spring_2025/Research/AtlasImager/AtlasImage/image.cpp \
-  /Users/jgibb/Documents/Spring_2025/Research/AtlasImager/AtlasImage/image.hpp
--- cmake-build-debug/CMakeFiles/d/eda572ebdc4c01ea0150be901e84749d4f2e06ff7d407fa2a117f0c4e8de272d.d
+++
@@ -1,5 +0,0 @@
-AtlasModel/Model_autogen/timestamp: \
-  /Applications/CLion.app/Contents/bin/cmake/mac/aarch64/bin/cmake \
-  /Users/jgibb/Documents/Spring_2025/Research/AtlasImager/AtlasModel/CMakeLists.txt \
-  /Users/jgibb/Documents/Spring_2025/Research/AtlasImager/AtlasModel/model.cpp \
-  /Users/jgibb/Documents/Spring_2025/Research/AtlasImager/AtlasModel/model.hpp
